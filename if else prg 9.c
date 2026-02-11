55c4fb032ef9a5afdf71de97d2f78caf9eb52624
rohithshanmugam <rohithshanmugam2007@gmail.com>
2026-02-11 16:22:48 +0530

    Add files via upload

---
+++ if else prg 9.c
@@ -0,0 +1,13 @@
+ #include<stdio.h>
+  int main()
+  {
+      int a;
+      scanf("%d",&a);
+      if(a>=18){
+          printf("Eligible for voting");
+      }
+      else{
+          printf(" Not Eligible for voting");
+      }
+      return 0;
+  }
