3f4648e2411abc916c4e1e9c61c17a47c2680cf5
rohithshanmugam <rohithshanmugam2007@gmail.com>
2026-02-11 13:56:34 +0530

    Add files via upload

---
+++ if else prg4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a<b){
+        printf("%d is greater",b);
+        
+    }
+    else{
+    printf("%d is not greater",a);
+}
+return 0;
+}
