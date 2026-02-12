aa191343cddfa9100e6337ef8f7df13dfebfbcf2
rohithshanmugam <rohithshanmugam2007@gmail.com>
2026-02-12 09:54:54 +0530

    Add files via upload

---
+++ if else prg 11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if(a>=90){
+        printf("Grade A");
+    }
+    else if(a>=75){
+        printf("Grade B");
+    }
+    else {
+        printf("Grade C");
+    }
+    return 0;
+}
