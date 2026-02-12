b78ed1eb394719e24590a687b79d09007cc858c6
rohithshanmugam <rohithshanmugam2007@gmail.com>
2026-02-12 10:43:40 +0530

    Update if else prg 11.c

--- if else prg 11.c
+++ if else prg 11.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int a;
-    scanf("%d", &a);
-    if(a>=90){
-        printf("Grade A");
+   int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b && a>c){
+        printf("%d is largest",a);
     }
-    else if(a>=75){
-        printf("Grade B");
-    }
-    else {
-        printf("Grade C");
-    }
+else if(b>c){
+    printf("%d is largest", b);
+}
+else
+    printf("%d is largest", c);
     return 0;
+
 }
