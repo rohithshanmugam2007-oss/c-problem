91f607eeadec571eba0b4ddbf64698ae118fe35a
rohithshanmugam <rohithshanmugam2007@gmail.com>
2026-02-16 13:16:25 +0530

    Delete f.loop prg21.c

--- f.loop prg21.c
+++
@@ -1,1 +0,0 @@
-#include<stdio.h>#include<math.h>int main(){    int a,b=0;    scanf("%d",&a);       for(int i=0;a!=0;a=a/10){        if (a%10!=0)            b+=pow(2,i);           i++;         }        printf("%d",b);        return 0;    }        
