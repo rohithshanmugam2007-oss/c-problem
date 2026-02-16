aefa28aa3ca691545e30de57bd0debab2dd86d59
rohithshanmugam <rohithshanmugam2007@gmail.com>
2026-02-16 14:14:18 +0530

    Add files via upload

---
+++ f.loop prg 14.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,c=0;
+    scanf("%d",&n);
+    for(int i=0;n!=0;n/=10){
+        if(n%2==0)
+        c=c+1;
+       
+        
+       
+}
+printf("%d",c);
+    
+
+return 0;
+}
