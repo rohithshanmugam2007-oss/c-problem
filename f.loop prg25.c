d6184fbdf8b2de7638da4477ca3a349d42e17ec1
rohithshanmugam <rohithshanmugam2007@gmail.com>
2026-02-16 15:19:39 +0530

    Add files via upload

---
+++ f.loop prg25.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d", &a);
+   int temp=a;
+   int c=0,b;
+    for(int i=1;a!=0;){
+        b=a%10;
+        c=c+b*b*b;;
+        a=a/10;
+    }
+    if(temp== c){
+        printf("Yes");
+    }
+   else
+        printf("No");
+    
+    
+    return 0;
+}
