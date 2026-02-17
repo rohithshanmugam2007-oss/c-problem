e748c3766d134d854ff3f8bb730b6e25387d9043
rohithshanmugam <rohithshanmugam2007@gmail.com>
2026-02-17 11:52:52 +0530

    Add files via upload

---
+++ f.loop.2 prg3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c=0;
+    scanf("%d\n%d",&a,&b);
+    
+        for(int i=1;i<=b;i++){
+          
+            c=i*100;
+            
+        }
+        printf("%d",a-c);
+    
+    
+    
+    return 0;
+}
