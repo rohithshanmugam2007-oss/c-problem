196a08c47c45c76aa0c4e1a050c54461c6e8ef14
rohithshanmugam <rohithshanmugam2007@gmail.com>
2026-02-17 11:27:09 +0530

    Add files via upload

---
+++ f.loop.3 prg.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c=0;
+    scanf("%d",&a);
+    for(int i=0;i<a;i++){
+        scanf("%d",&b);
+       c=c+b;
+    }
+    printf(" Total units:%d",c);
+   
+    
+    
+    
+    
+    return 0;
+}
