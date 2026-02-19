99672e42822261a9b2b188686ecd63c521269aba
rohithshanmugam <rohithshanmugam2007@gmail.com>
2026-02-19 09:55:47 +0530

    Add files via upload

---
+++ f.loop.2 prg20.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+     int a,b,c,d=0;
+     int count=0;
+     scanf("%d%d",&a,&b);
+     for(int i=0;i<b;i++){
+         scanf("%d",&c);
+         d=d+c;
+         if(d>a){
+             count++;
+         }
+         
+     }
+     printf("Remaining Capacity:%d\n",a-d);
+     printf("Overflow Days:%d",count);
+     return 0;
+}
