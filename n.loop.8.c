2d7cef358d0cc3029c798f8079655ff4ff1a7f81
rohithshanmugam <rohithshanmugam2007@gmail.com>
2026-03-02 13:58:01 +0530

    Add files via upload

---
+++ n.loop.8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    for(int i=0;i<a;i++){
+        for(int j=0;j<a-i;j++){
+            printf("%c",65+j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
