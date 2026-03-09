7da7996844f6e8ad14350b8a76a1de57ddda3f42
rohithshanmugam <rohithshanmugam2007@gmail.com>
2026-03-09 11:44:43 +0530

    Add files via upload

---
+++ 2arr.10.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int a,b,m=1,max=1000,index=100,i;
+    int count=0;
+    scanf("%d %d",&a,&b);
+    int arr[a][b];
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+     for(int i=0;i<a;i++){
+         m=1;
+         for(int j=0;j<b;j++){
+             m=m*arr[i][j];
+         }
+         if(m<max){
+             m=max;
+             index=i;
+         }
+     }
+     
+    
+    printf("%d",i);
+    
+    return 0;
+}
