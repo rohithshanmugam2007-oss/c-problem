2e4438c8298f5c9beae136eb453f5b85c4da7a3b
rohithshanmugam <rohithshanmugam2007@gmail.com>
2026-03-14 14:25:26 +0530

    Create str.15.c

---
+++ str.15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char a[100];
+    int count=1;
+    fgets(a, sizeof(a), stdin); 
+   for (int i=0;a[i]!='\0';i++){
+        if (a[i]==' '){
+            count++;
+        }
+  }
+    printf("%d",count);
+    return 0;
+}
